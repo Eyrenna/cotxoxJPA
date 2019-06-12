6891d6e0a3945cda672fad398f48f6ab08da3326
Eyrenna <claudia365x@gmail.com>
2019-06-12 19:42:40 +0200

    Mapping de Carrera y Conductor; pasa casos test

--- src/main/java/org/mvpigs/cotxox/domain/Carrera.java
+++ src/main/java/org/mvpigs/cotxox/domain/Carrera.java
@@ -1,16 +1,49 @@
 package org.mvpigs.cotxox.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_carreras")
 public class Carrera {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="c_id")
     private Long id;
+	
+	@Column(name="c_tarjeta_credito")
 	private String tarjetaCredito = null;
+	
+	@Column(name="c_origen")
 	private String origen = null;
+	
+	@Column(name="c_destino")
 	private String destino = null;
+	
+	@Column(name="c_distancia")
 	private double distancia = 0d;
+	
+	@Column(name="c_tiempo_esperado")
 	private int tiempoEsperado = 0;
+	
+	@Column(name="c_tiempo_carrera")
 	private int tiempoCarrera = 0;
+	
+	@Column(name="c_coste_total")
 	private double costeTotal = 0;
-	private int propina = 0;	
+	
+	@Column(name="c_propina")
+	private int propina = 0;
+	
+	@ManyToOne
+	@JoinColumn(name="c_conductor")
 	private Conductor conductor = null;
 	
 	/**
@@ -18,6 +51,8 @@ public class Carrera {
 	 * para trabajar con Spring JPA
 	 */
 
+	public Carrera() {}
+	
 	public Carrera(String tarjetaCredito){
 		this.tarjetaCredito = tarjetaCredito;
 	}
--- src/main/java/org/mvpigs/cotxox/domain/Conductor.java
+++ src/main/java/org/mvpigs/cotxox/domain/Conductor.java
@@ -4,15 +4,38 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="t_conductores")
 public class Conductor {
 
+	@Id
+	@Column(name="co_tarjeta_credito")
 	private String tarjeta;
+	
+	@Column(name="co_nombre")
 	private String nombre = null;
+	
+	@Column(name="co_modelo")
 	private String modelo = null;
+	
+	@Column(name="co_matricula")
 	private String matricula = null;
+	
+	@Column(name="co_valoracion_media")
 	private double valoracionMedia = 0d;
+	
+	@Column(name="co_ocupado")
 	private boolean ocupado = false;
+	
+	@Transient
 	private ArrayList<Byte> valoraciones = new ArrayList<>();
+	@Transient
 	private Set<Carrera> carreras = new HashSet<>();
 
 	/**
